48cf9e4260d2f2b2e3bcfc9c24f884dd1c6304b2
Mark Final <markfinal@hotmail.com>
2019-12-16 08:31:54 +0000

    [tests,ThreadTest1] Some Gcc and clang warning suppressions

--- tests/ThreadTest1/bam/Scripts/ThreadTest1.cs
+++ tests/ThreadTest1/bam/Scripts/ThreadTest1.cs
@@ -38,22 +38,27 @@ namespace ThreadTest1
                 if (settings is C.ICommonCompilerSettings compiler)
                 {
                     compiler.WarningsAsErrors = true;
-                }
-                if (settings is VisualCCommon.ICommonCompilerSettings vcCompiler)
-                {
-                    vcCompiler.WarningLevel = VisualCCommon.EWarningLevel.Level4;
-                }
-                if (settings is GccCommon.ICommonCompilerSettings gccCompiler)
-                {
-                    gccCompiler.AllWarnings = true;
-                    gccCompiler.ExtraWarnings = true;
-                    gccCompiler.Pedantic = true;
-                }
-                if (settings is ClangCommon.ICommonCompilerSettings clangCompiler)
-                {
-                    clangCompiler.AllWarnings = true;
-                    clangCompiler.ExtraWarnings = true;
-                    clangCompiler.Pedantic = true;
+                    if (settings is VisualCCommon.ICommonCompilerSettings vcCompiler)
+                    {
+                        vcCompiler.WarningLevel = VisualCCommon.EWarningLevel.Level4;
+                    }
+                    if (settings is GccCommon.ICommonCompilerSettings gccCompiler)
+                    {
+                        gccCompiler.AllWarnings = true;
+                        gccCompiler.ExtraWarnings = true;
+                        gccCompiler.Pedantic = true;
+                        if (source.Compiler.Version.AtLeast(GccCommon.ToolchainVersion.GCC_8))
+                        {
+                            compiler.DisableWarnings.AddUnique("parentheses");
+                        }
+                    }
+                    if (settings is ClangCommon.ICommonCompilerSettings clangCompiler)
+                    {
+                        clangCompiler.AllWarnings = true;
+                        clangCompiler.ExtraWarnings = true;
+                        clangCompiler.Pedantic = true;
+                        compiler.DisableWarnings.AddUnique("unused-parameter");
+                    }
                 }
             });
 
